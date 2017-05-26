f55dd6b645785180372d1e562e935bbe3f25b75c
冷洪林 <18708363472@qq.com>
2017-05-26 17:58:41 +0800

    学习到字符串

--- LearningSwift.playground/Contents.swift
+++ LearningSwift.playground/Contents.swift
@@ -36,17 +36,77 @@ _ = {
 }
 
 // 4.逻辑分支
-// * 1>.Swift中if的写法:
-// * if后面的()可以省略
-// * 判断句不再有非0/nil即真,判断句必须有明确的真假(Bool-->true/false)
-let a = 10
-if a > 0 {
-    print("a大于0")
-}else{
-    print("a小于0")
+_ = {
+    // * 1>.Swift中if的写法:
+    // * if后面的()可以省略
+    // * 判断句不再有非0/nil即真,判断句必须有明确的真假(Bool-->true/false)
+    let a = 10
+    if a > 0 {
+        print("a大于0")
+    }else{
+        print("a小于0")
+    }
+    // * 2>.Swift中else if的写法
+    // * 与if写法一致
+    let score = 85
+    if score < 0 || score > 100{
+        print("非法的程序")
+    }else if score < 60 {
+        print("不及格")
+    }else if score < 80 {
+        print("及格")
+    }else if score <= 100 {
+        print("牛逼")
+    }
+    // * 3>.三目运算符
+    let m = 10, n = 15
+    let result = m > n ? m : n
+    // * 4>.guard运算
+    let age = 20
+    let money = 0
+    let IDCard = true
+    func onLine (age : Int, money : Int, IDCard: Bool) {
+        guard age > 18 else {
+            print("未成年禁止上网")
+            return
+        }
+        guard money >= 0 else {
+            print("没有钱禁止上网")
+            return
+        }
+        guard IDCard else {
+            print("没有身份证禁止上网")
+            return
+        }
+        print("开卡上网")
+    }
+    
+    onLine(age: age, money: money, IDCard: IDCard)
+    // * 4>.switch运算
+    // ...
 }
-// * 2>.Swift中else if的写法
-// * 与if写法一致
+
+// 5.字符串
+// 1>.定义字符串
+let str : String = "+"
+let str1 = "冷洪林"
+// 2>.遍历字符串
+for name in str1.characters {
+    print(name)
+}
+// 3>.字符串拼接
+let str2 = "lhl"
+let str3 = "lyx"
+let str4 = str3 + str2
+
+let str5 = "lhl"
+let age = 20
+let height = 1.88
+let info = "\(str5)的年龄是\(age), 身高是\(height)"
+
+let min = 2
+let second = 18
+let timeStr = String(Formatter )
 
 
 
